74eee959b98b45d5bb2ca84ce808880ed42ffbc5
salihkilicoglu <salihklicoglu@gmail.com>
2021-12-05 17:17:14 +0300

    api/movies/:movie_id[GET]

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api/movie', movieRouter);
+app.use('/api/movies', movieRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- routes/movie.js
+++ routes/movie.js
@@ -4,26 +4,35 @@ const router = express.Router();
 // Models
 const Movie = require('../models/Movie');
 
+router.get('/', (req, res) => {
+  const promise = Movie.find({ });
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+router.get('/:movie_id', (req, res) => {
+  const promise = Movie.findById(req.params.movie_id);
+  promise.then((movie) => {
+    res.json(movie);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
 router.post('/', (req, res, next) => {
   // const { title, imdb_score, category, country, year } = req.body;
 
   const movie = new Movie(req.body);
-
-  /*movie.save((err, data) => {
-    if (err)
-      res.json(err);
-    
-    res.json({ status: 1 });
-  });*/
-
   const promise = movie.save();
   
   promise.then((data) => {
     res.json({ status: 1 });
   }).catch((err) => {
     res.json(err);
-  })
-
+  });
 });
 
 module.exports = router;
