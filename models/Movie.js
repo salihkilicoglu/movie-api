324824627d52e798dcdf9cc9e7a6305d4dafc800
salihkilicoglu <salihklicoglu@gmail.com>
2021-12-05 20:16:31 +0300

    MovieSchema validation

--- models/Movie.js
+++ models/Movie.js
@@ -5,12 +5,30 @@ const MovieSchema = new Schema({
     director_id: Schema.Types.ObjectId,
     title:{
         type: String,
-        required: true
+        required: [true, '`{PATH} alanı zorunludur.`'],
+        maxlength: [15, '`{PATH}` alanı (`{VALUE}`), ({MAXLENGTH}) karakterden küçük olmalıdır.'],
+        minlength: [3, '`{PATH}` alanı (`{VALUE}`), ({MINLENGTH}) karakterden büyük olmalıdır.']
     },
-    category: String,
-    country: String,
-    year: Number,
-    imdb_score: Number,
+    category: {
+        type: String,
+        maxlenght: 30,
+        minLength: 1
+    },
+    country: {
+        type: String,
+        maxlenght: 30,
+        minLength: 1 
+    },
+    year: {
+        type: Number,
+        max: 2040,
+        min: 1900
+    },
+    imdb_score: {
+        type: Number,
+        max: 10,
+        min: 0
+    },
     CreatedAt: {
         type: Date,
         default: Date.now
