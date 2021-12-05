1badc75148fbe3ee6933d93f94d6e3f6c9573243
salihkilicoglu <salihklicoglu@gmail.com>
2021-12-05 16:32:29 +0300

    movie.save with callbacks

--- routes/movie.js
+++ routes/movie.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.json({ status: 1 });
+// Models
+const Movie = require('../models/Movie');
+
+router.post('/', (req, res, next) => {
+  // const { title, imdb_score, category, country, year } = req.body;
+
+  const movie = new Movie(req.body);
+
+  movie.save((err, data) => {
+    if (err)
+      res.json(err);
+    
+    res.json({ status: 1 });
+  });
+
 });
 
 module.exports = router;
