cf3edf7f1764fe5d7fe887e136eb696234e998db
salihkilicoglu <salihklicoglu@gmail.com>
2021-12-13 22:28:10 +0300

    /api/movies tests

--- helper/db.js
+++ helper/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 module.exports = () => {
     mongoose.connect('mongodb+srv://movie_user:abcd1234@movie-api.cyxkz.mongodb.net/movie-api?retryWrites=true&w=majority');
     mongoose.connection.on('open', () => {
-        console.log('MongoDB: Connected');
+        //console.log('MongoDB: Connected');
     });
     mongoose.connection.on('error', (err) => {
         console.log('MongoDB: Error', err);
